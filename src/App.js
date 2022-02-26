1f1bdb8698e7256142fce3cf46e71831ae8bae39
ayebravo <ayelenbravo@gmail.com>
2022-02-25 18:26:53 -0600

    Updated client app so user can specify limit and start params

--- src/App.js
+++ src/App.js
@@ -7,14 +7,25 @@ import { API } from "aws-amplify";
 import "./App.css";
 
 const App = () => {
+	// Create additional state to hold user input for limit and start properties
+	const [input, updateInput] = useState({ limit: 5, start: 0 });
+
+	// Create a new function to allow users to update the input values
+	const updateInputValues = (type, value) => {
+		updateInput({ ...input, [type]: value });
+	};
+
 	// Create coins variable and set to empty array
 	const [coins, updateCoins] = useState([]);
 
 	// Define function to all API
 	// async function fetchCoins() { // Reformated to what it's in line 15
 	const fetchCoins = async () => {
-		const data = await API.get("cryptoapi", "/coins");
-		console.log(data);
+		const { limit, start } = input;
+		const data = await API.get(
+			"cryptoapi",
+			`/coins?limit=${limit}&start=${start}`
+		);
 		updateCoins(data.coins);
 	};
 
@@ -25,6 +36,17 @@ const App = () => {
 
 	return (
 		<div className="App">
+			<input
+				onChange={(e) => updateInputValues("limit", e.target.value)}
+				placeholder="Enter a limit"
+			/>
+			<input
+				placeholder="Enter a starting value"
+				onChange={(e) => updateInputValues("start", e.target.value)}
+			/>
+
+			<button onClick={fetchCoins}>Fetch Coins</button>
+
 			{coins.map((coin) => (
 				<div key={coin.name}>
 					<h2>
